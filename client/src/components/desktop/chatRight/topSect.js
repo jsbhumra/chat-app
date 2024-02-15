9e0bdc778af268558043e2e364f3c0d299e14457
jsbhumra <jagjit.s.bhumra@gmail.com>
2024-02-15 17:44:33 +0530

    Some more changes (client+server)

--- client/src/components/desktop/chatRight/topSect.js
+++ client/src/components/desktop/chatRight/topSect.js
@@ -10,13 +10,13 @@ function TopSect({ socket, chat }) {
         setUserName(localStorage.getItem("userName"))
         setAvatar(localStorage.getItem("avatar"))
         socket.on('chatDisc', ()=>{setUserName("");setAvatar("");})
-    },[socket,chat])
+    },[chat])
 
   return (
     <div className='min-h-[35px] h-[35px] shadow-[0_10px_7.5px_2px_#ccddff] shadow-[#ccddff] overflow-x-hidden w-full flex z-[102] justify-between'>
         <div className='flex h-full w-auto items-center justify-start bg-white px-5 rounded-full border border-4 border-lime-500 text-lg'>
             <div className='line-clamp-1'>
-            <span>You are chatting as: </span>&nbsp;<span className='font-bold'>{userName}</span>
+            <span>You are chatting as: </span>&nbsp;<span className='font-bold'>{localStorage.getItem("userName")}</span>
             </div>
         </div>
         <div className='h-full min-w-[120px] flex itmes-center justify-center'>
