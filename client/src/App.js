ab233f93a1f73089627e8529b9061eabd15fa132
jsbhumra <jagjit.s.bhumra@gmail.com>
2024-02-15 23:59:14 +0530

    Last simple commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Chat from './pages/chat';
 import socketIO from 'socket.io-client';
-const socket = socketIO.connect(process.env.REACT_BACKEND_URL);
+const socket = socketIO.connect(process.env.REACT_BACKEND_URL || "http://localhost:3001");
 
 function App() {
   return (
